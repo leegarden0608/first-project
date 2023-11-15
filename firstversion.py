4c60a663ce205696eab9b0a495cea66c09de86f9
leegarden <leegarden0608@gmail.com>
2023-11-15 20:05:07 +0900

    fourth commit

--- firstversion.py
+++ firstversion.py
@@ -2,6 +2,21 @@ print("Hello World")
 print("new word")
 
 a = 1
+b = 2
 for i in range(5):
     a += i
-    print(a)
+    b = b* i
+    print(a, b)
+    
+
+'''
+새로운거 추가 하는 법(git)
+
+git add .
+
+git commit -m "~번째 commit"
+
+git push origin master
+
+
+'''
